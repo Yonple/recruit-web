b29cff6bdaa3dc88fe9585e1567bafe722391dea
Beygee <doug0476@naver.com>
2021-06-26 12:10:04 +0900

    feat: inject token to api

--- src/api/index.ts
+++ src/api/index.ts
@@ -14,14 +14,16 @@ export interface FindPostsRequest {
   page?: number
 }
 
+const TOKEN = '018526'
+
 export const findPosts = async ({ type, query, page }: FindPostsRequest) => {
   const { data } = await apiAxios.get<Post[]>(
-    `/${type}-posts?page=${page ?? 0}&search=${query ?? ''}`,
+    `/recruit/${TOKEN}/${type}-posts?page=${page ?? 0}&search=${query ?? ''}`,
   )
   return data
 }
 
 export const findPost = async (id: string, type: 'a' | 'b') => {
-  const { data } = await apiAxios.get<Post>(`/${type}-posts/${id}`)
+  const { data } = await apiAxios.get<Post>(`/recruit/${TOKEN}/${type}-posts/${id}`)
   return data
 }
